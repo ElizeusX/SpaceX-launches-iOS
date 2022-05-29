6bb68116ea73169c664a067f6bae2d9b91163325
Elizeus <elizeus.chrabrov@juicymo.cz>
2022-05-29 12:18:04 +0200

    Add receiving launch data and reload collection view by use Combine

--- SpaceX-launches-MVC-iOS/Controllers/LaunchListViewController.swift
+++ SpaceX-launches-MVC-iOS/Controllers/LaunchListViewController.swift
@@ -6,22 +6,44 @@
 //
 
 import UIKit
+import Combine
 
 class LaunchListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    private var launchService = LaunchesService()
+    private var launchDataSubscriber: AnyCancellable?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        launchService.fetchData()
+        reloadCollectionView()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.launchDataSubscriber?.cancel()
+    }
+
+    func reloadCollectionView(){
+        self.launchDataSubscriber = launchService.$launchData.sink { data in
+            if !data.isEmpty {
+                self.collectionView.reloadData()
+            }
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9 // ToDo: For test
+        return self.launchService.launchCount ?? 0
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let launchCell: LaunchCellView = collectionView.dequeueReusableCell(withReuseIdentifier: "launchCell", for: indexPath) as! LaunchCellView
+        let cell: LaunchCellView = collectionView.dequeueReusableCell(withReuseIdentifier: "launchCell", for: indexPath) as! LaunchCellView
+        // (For test) TODO: correctly display cell
+        cell.NameLabel?.text = launchService.cellItem(for: indexPath.row).name
 
-        return launchCell
+        return cell
     }
 }
 
