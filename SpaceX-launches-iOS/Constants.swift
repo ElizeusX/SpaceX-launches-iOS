81e52077263d60c29974ff2ad43932b83ca1b9d6
Elizeus <elizeus.chrabrov@juicymo.cz>
2022-06-08 22:53:56 +0200

    Add icons in constants and change url for launches

--- SpaceX-launches-iOS/Constants.swift
+++ SpaceX-launches-iOS/Constants.swift
@@ -12,9 +12,22 @@ struct Constants{
     struct URLs {
         static let versionApi = "v4"
         static let baseUrl = "https://api.spacexdata.com/\(versionApi)"
-        static let launchesUrl = "\(baseUrl)/launches"
+        static let launchesUrl = "\(baseUrl)/launches/past"
         static let rocketsUrl = "\(baseUrl)/rockets"
     }
 
     static let headers: HTTPHeaders = ["Content-Type": "application/json"]
+
+    struct Icons {
+        static let success = "checkmark.circle"
+        static let fail = "x.circle"
+        static let calendar = "calendar"
+        static let mass = "scalemass"
+        static let orderOfPriority = "arrow.up.arrow.down"
+        static let height = "arrow.up.and.down"
+        static let info = "info.circle"
+        static let questionmark = "questionmark.circle"
+        static let xmark = "xmark"
+
+    }
 }
