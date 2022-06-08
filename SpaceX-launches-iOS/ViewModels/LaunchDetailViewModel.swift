f16a556a3fef13c77841104ece1902d9977f55ea
Elizeus <elizeus.chrabrov@juicymo.cz>
2022-06-08 23:15:19 +0200

    Add labels for launch detail

---
+++ SpaceX-launches-iOS/ViewModels/LaunchDetailViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  LaunchDetailViewModel.swift
+//  SpaceX-launches-iOS
+//
+//  Created by Elizeus on 05.06.2022.
+//
+
+import Foundation
+import UIKit
+
+class LaunchDetailViewModel {
+
+    var launchData: LaunchData?
+    var rocketData: RocketData?
+
+    func successColor() -> UIColor {
+        guard let success = launchData?.success else { return .gray }
+        return success ? .green : .red
+    }
+
+    func successIcon() -> String {
+        guard let success = launchData?.success else { return Constants.Icons.questionmark}
+        return success ? Constants.Icons.success : Constants.Icons.fail
+    }
+}
--- SpaceX-launches-iOS/Views/LaunchDetail/LaunchDetailViewController.swift
+++ SpaceX-launches-iOS/Views/LaunchDetail/LaunchDetailViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 class LaunchDetailViewController: UIViewController {
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var successLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
 
     let viewModel = LaunchDetailViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLabels()
+    }
+
+    func setupLabels() {
+        guard let launchData = viewModel.launchData else { return }
+
+        nameLabel.text = launchData.name
+        numberLabel.text = "#\(launchData.flightNumber ?? 0)"
+        successLabel.addLeading(
+            image: UIImage(systemName: viewModel.successIcon()) ?? UIImage(),
+            text: "Success",
+            imageColor: viewModel.successColor()
+        )
+        dateLabel.addLeading(
+            image: UIImage(systemName: Constants.Icons.calendar) ?? UIImage(),
+            text: DateService.convertStringToDate(from: launchData.dateUtc ?? "") ?? "",
+            imageColor: .white
+        )
+        detailLabel.addLeading(
+            image: UIImage(systemName: Constants.Icons.info) ?? UIImage(),
+            text: launchData.details ?? "",
+            imageColor: .white
+        )
+        imageView.downloaded(from: launchData.links?.patch?.small ?? "")
     }
 }
